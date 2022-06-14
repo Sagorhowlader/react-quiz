c2d3fef271f9a0b3b7de699ae12c32e1a898b737
sagor2 <sagor@workspaceit.com>
2022-06-15 00:57:14 +0600

    show video in home page

---
+++ src/components/Videos.js
@@ -0,0 +1,9 @@
+import Video from "../components/Video";
+import classes from "../styles/videos.module.css";
+export default function Videos() {
+  return (
+    <div class={classes.videos}>
+      <Video />
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import Videos from "../components/Videos";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return <Videos />;
 }
